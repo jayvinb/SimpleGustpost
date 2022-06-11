6306fc0b1a3a05293f1065491c007376a5fbda4d
jayvin-jhavtech <jayvin@shoptype.com>
2022-06-11 12:34:42 +0530

    This is a new commit for what I originally planned to be amended

---
+++ simplegustpost.php
@@ -0,0 +1,16 @@
+<?php
+
+defined('ABSPATH') or die('No script kiddies please');
+
+/*
+  Plugin Name: Simple gust post
+  Description: A plugin to submit and manage WordPress posts from frontend with or without logging in
+  Version:     1.0.0
+  Author:      Jayvin busa
+  Author URI:  
+  Plugin URI: 
+  License:     GPL2
+  License URI: https://www.gnu.org/licenses/gpl-2.0.html
+  Domain Path: /languages
+  Text Domain: simple-gust-post
+ */
