1a018df48e6260d3b5afc4d9f2f0043a38681601
jayvin-jhavtech <jayvin@shoptype.com>
2022-06-15 18:01:09 +0530

    Write snippet for Ajax form submit

--- simplegustpost.php
+++ simplegustpost.php
@@ -26,33 +26,30 @@ class SGP_GuestPostSubmit{
 	wp_enqueue_script('tinymce_min', includes_url('js/tinymce/tinymce.min.js',__FILE__));
 	wp_enqueue_script('tiny_mce', plugins_url('tiny_mce.js',__FILE__));
 	*/
-	if (is_admin()){
-        
-	    add_action( 'admin_menu', array($this, 'sgp_add_settings_menu') );
-        add_action( 'admin_init', array($this, 'sgp_init_settings') );
+if (is_admin()){
+    add_action( 'admin_menu', array($this, 'sgp_add_settings_menu') );
+    add_action( 'admin_init', array($this, 'sgp_init_settings') );
 	}
 	$this->options = get_option( 'sgp_options' );
     $this->enable_shortcode();
-	add_action( 'template_redirect', array($this, 'sgp_template_redirection')  );
+	//add_action( 'template_redirect', array($this, 'sgp_template_redirection')  );
    }
     
-
-    public function sgp_template_redirection( $template ) {	
-	if ( !empty( $_POST['sgp_form_submitted'] ) ) {	    
-	    $this->sgp_process_submit_form();
-	} else {
-	    return $template;
-	}		
-    }
+// public function sgp_template_redirection( $template ) {	
+// 	if ( !empty( $_POST['sgp_form_submitted'] ) ) {	    
+// 	    $this->sgp_process_submit_form();
+// 	} else {
+// 	    return $template;
+// 	}		
+//     }
     
-    public function sgp_add_settings_menu() {
-	//add_options_page( __('SGP Guest Post Submit Options', 'Gust Posts'), __('SGP Guest Post Submit', 'Gust Posts'), 'administrator', __FILE__, array($this, 'sgp_display_menu_page') );
+public function sgp_add_settings_menu() {
+    //add_options_page( __('SGP Guest Post Submit Options', 'Gust Posts'), __('SGP Guest Post Submit', 'Gust Posts'), 'administrator', __FILE__, array($this, 'sgp_display_menu_page') );
 	add_options_page( 'SGP Guest Post Submit Options', 'SGP Guest Post Submit', 'administrator', __FILE__, array($this, 'sgp_display_menu_page') );
     }
     
-    public function sgp_display_menu_page(){
-
-	?>
+public function sgp_display_menu_page(){
+    ?>
 	<div id="tt-general" class="wrap">
             <h2><?php _e('SGP Guest Post Submit Options','Gust Posts'); ?></h2>
             <div id="short-code">Shortcode for this plugin: [sgp-submit-post]</div>
@@ -65,24 +62,23 @@ class SGP_GuestPostSubmit{
 	<?php
     }
     
-    public function sgp_init_settings(){
-	    register_setting( 'sgp_settings', 'sgp_options');
-		add_settings_section( 'sgp_general_settings_section', __('General Settings', 'sgp_text_domain'), array($this, 'sgp_general_setting_section_callback'), 'sgp_settings_section' );
+public function sgp_init_settings(){
+	register_setting( 'sgp_settings', 'sgp_options');
+	add_settings_section( 'sgp_general_settings_section', __('General Settings', 'sgp_text_domain'), array($this, 'sgp_general_setting_section_callback'), 'sgp_settings_section' );
 	
 	/*GENERAL SESGPINGS*/
-        add_settings_field( 'sgp_txt_contact_email', __('Email for Notification', 'sgp_text_domain'), array($this,'sgp_display_text_field'), 'sgp_settings_section', 'sgp_general_settings_section', array( 'name' => 'sgp_txt_contact_email', 'txt_type' => 'email', 'place_holder' =>'Email Address For Sending Notification', 'size'=>50  ) );
-        add_settings_field( 'sgp_txt_confirmation_msg', __('Post Submit Confirmation Message', 'sgp_text_domain'), array($this,'sgp_display_text_field'), 'sgp_settings_section', 'sgp_general_settings_section', array( 'name' => 'sgp_txt_confirmation_msg', 'txt_type' => 'text', 'place_holder' =>'Type Message To Show When Post Submit Successfull', 'size'=>50  ) );
-        add_settings_field( 'sgp_txt_failure_msg', __('Post Submit Failure Message', 'sgp_text_domain'), array($this,'sgp_display_text_field'), 'sgp_settings_section', 'sgp_general_settings_section', array( 'name' => 'sgp_txt_failure_msg', 'txt_type' => 'text', 'place_holder' =>'Type Message To Show When Post Submit Fails', 'size'=>50  ) );
+    add_settings_field( 'sgp_txt_contact_email', __('Email for Notification', 'sgp_text_domain'), array($this,'sgp_display_text_field'), 'sgp_settings_section', 'sgp_general_settings_section', array( 'name' => 'sgp_txt_contact_email', 'txt_type' => 'email', 'place_holder' =>'Email Address For Sending Notification', 'size'=>50  ) );
+    add_settings_field( 'sgp_txt_confirmation_msg', __('Post Submit Confirmation Message', 'sgp_text_domain'), array($this,'sgp_display_text_field'), 'sgp_settings_section', 'sgp_general_settings_section', array( 'name' => 'sgp_txt_confirmation_msg', 'txt_type' => 'text', 'place_holder' =>'Type Message To Show When Post Submit Successfull', 'size'=>50  ) );
+    add_settings_field( 'sgp_txt_failure_msg', __('Post Submit Failure Message', 'sgp_text_domain'), array($this,'sgp_display_text_field'), 'sgp_settings_section', 'sgp_general_settings_section', array( 'name' => 'sgp_txt_failure_msg', 'txt_type' => 'text', 'place_holder' =>'Type Message To Show When Post Submit Fails', 'size'=>50  ) );
         }
-        public function sgp_general_setting_section_callback() {
-            echo "<p class='fullv-msg'>Thank You For Using SGP gust post plugin";
-            
+public function sgp_general_setting_section_callback() {
+    echo "<p class='fullv-msg'>Thank You For Using SGP gust post plugin";
         }
-    public function sgp_display_text_field( $data = array() ) {
-	    extract( $data );
+public function sgp_display_text_field( $data = array() ) {
+	extract( $data );
 	//$options = get_option( 'sgp_options' ); 
 	?>
-        <input type="<?php echo $txt_type ?>" name="sgp_options[<?php echo $name; ?>]" placeholder="<?php echo $place_holder; ?>" <?php if($txt_type=="number"){echo ' min="0"';} ?> size="<?php echo $size; ?>" <?php echo " ".$disabled; ?> value="<?php echo esc_html( $this->options[$name] ); ?>"/>
+    <input type="<?php echo $txt_type ?>" name="sgp_options[<?php echo $name; ?>]" placeholder="<?php echo $place_holder; ?>" <?php if($txt_type=="number"){echo ' min="0"';} ?> size="<?php echo $size; ?>" <?php echo " ".$disabled; ?> value="<?php echo esc_html( $this->options[$name] ); ?>"/>
 	<?php
         if($second_field){
             ?>
@@ -136,8 +132,7 @@ class SGP_GuestPostSubmit{
 	}
 
 	//Post variable to indicate user-submitted items
-	$template_str .= '<input type="hidden" name="sgp_form_submitted" value="1" />';
-        $template_str .= '<form id="sgp-form" action="" method="post" enctype="multipart/form-data">
+	    $template_str .= '<form id="sgp-form" method="post" enctype="multipart/form-data">
 			    <div id="wrapping" class="clearfix">
                                 <section id="aligned">';
                                             
@@ -153,31 +148,23 @@ class SGP_GuestPostSubmit{
 					$template_str .= 'placeholder="'.__("Write Your Post Expert", "Gust Posts").'"></textarea>';
 					$template_str .= '<p id="fi-title">'. __("Upload Featured Image and Additional Images","Gust Posts") . '</p>
 							    <div class="featured-img postfield">
-								<input name="featured-img[]" type="file" id="featured-img"';
-								$template_str .= ' required="required" ';
-								$template_str .= ' multiple="multiple"><br>
+								<input name="featured-img" type="file" id="featured-img"';
+					$template_str .= ' required="required" ';
+					$template_str .= ' multiple="multiple"><br>
 							    </div>';
 				    
-				    $template_str .= '<input type="hidden" value="'. $author .'" name="authorid">
-							  <input type="hidden" value="'. $redirect_url .'" name="redirect_url">
-							  <input type="hidden" value="'. $this->options["sgp_drp_status"] .'" name="post_status">
-							  
-							  <input type="hidden" value="';
-				    $template_str .= isset($this->options["sgp_chk_notifyfield"])?$this->options["sgp_chk_notifyfield"]:"";
-				    $template_str .= '" name="notify_flag">
-							  
-	
-							  
-							  <input type="hidden" value="'. $to_mail .'" name="to_email">
-							  <input type="hidden" name="sgp_form_submitted" value="1" />' . 
+				    $template_str .= '<input type="hidden" value="on" name="notify_flag">';
+
+				    $template_str .= '<input type="hidden" value="'. $to_mail .'" name="to_email">' . 
                                 '</section>
                                 <section id="buttons">
-                                        <input type="reset" name="reset" id="resetbtn" class="resetbtn" value="'.__("Reset", "Gust Posts").'">
-                                        <input type="submit" name="submit" id="submitbtn" class="submitbtn" tabindex="7" value="'.__("Submit Post", "Gust Posts").'">
+                                        <input type="reset" name="reset" id="resetbtn" class="resetbtn" value="reset">
+                                        <input type="submit" name="submit" id="submit" class="submitbtn" tabindex="7" value="submit">
                                         <br style="clear:both;">
                                 </section>
                             </div>
                         </form>';
+                         
          return $template_str; 
 }
     
@@ -188,11 +175,8 @@ class SGP_GuestPostSubmit{
 	    return "";
 	}
 }
-    
-    
-    public function sgp_process_submit_form(){
-	submit_post_function();
-    } // End of function
+
+
 
 } // End of Class
 add_action( 'init', 'sgp_plugin_init' );
@@ -205,7 +189,7 @@ function sgp_plugin_init() {
 add_action( 'init', 'add_gust_post_type');
 
     //add gust post post type to manage all gust post
-    function add_gust_post_type() {
+function add_gust_post_type() {
         //labels array added inside the function and precedes args array
         
         $labels = array(
@@ -238,4 +222,49 @@ add_action( 'init', 'add_gust_post_type');
         register_post_type( 'gust_post', $args );
         }
 
-?>
+        //rafister hook for admin ajex
+        add_action('wp_ajax_new_submit_post','submit_post_function');
+        add_action( 'wp_ajax_nopriv_new_submit_post', 'submit_post_function' ); 
+        
+        function footer_ajax_script(){ ?>
+        
+        <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+                        <script>
+                        
+                            jQuery('#sgp-form').on('submit', function ()
+                            {
+
+                                event.preventDefault();
+                                var files = jQuery('#featured-img')[0].files;
+                                var link="<?php echo admin_url('admin-ajax.php')?>";
+                                var form=jQuery('#sgp-form').serialize();
+                                var formData=new FormData;
+                                formData.append('action','new_submit_post');
+                                formData.append('new_submit_post',form);
+                                formData.append('featured-img',files[0]);
+                                console.log(files[0]);
+                                jQuery.ajax(
+                                {
+                                  url:link,
+                                  data:formData, 
+                                  processData:false,
+                                  contentType:false,
+                                  type:'post',
+                                  success: function (response) {
+                                        console.log(response);
+                                        jQuery('#resetbtn').click()
+                                    }, 
+                                  fail: function (err) {
+                                        alert("There was an error: " + err);
+                                    }
+                                 
+                                }); 
+                            });
+                          </script>          
+        <?php } 
+        
+//Add javascript in wordpress footer 
+add_action('wp_footer', 'footer_ajax_script'); 
+        
+        ?>
+
